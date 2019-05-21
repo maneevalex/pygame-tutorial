319539b49f160aeeb731e91478c63dcb01ed3d89
maneevalex <43039650+maneevalex@users.noreply.github.com>
2019-05-21 13:12:38 +0300

    Add rectangle as a player

--- game.py
+++ game.py
@@ -19,4 +19,19 @@ while True:
         if event.type == pygame.QUIT:
             run = False
 
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_LEFT]:
+        x -= vel
+    if keys[pygame.K_RIGHT]:
+        x += vel
+    if keys[pygame.K_UP]:
+        y -= vel
+    if keys[pygame.K_DOWN]:
+        y += vel
+
+    win.fill((0, 0, 0))
+    pygame.draw.rect(win, (255, 0, 0),(x, y, width, height))
+    pygame.display.update()
+
 pygame.quit()
